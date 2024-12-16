3dbd357db38454e4f859291019c2f527e4432191
BugHunter-X <askzenai@gmail.com>
2024-12-16 17:48:06 +0100

    Add bug.lua file

---
+++ bug.lua
@@ -0,0 +1,13 @@
+local function foo(x)
+  if x == nil then
+    return nil  -- Correctly handles nil input
+  elseif type(x) ~= 'number' then
+    error('Invalid input: x must be a number')  -- Explicit error handling
+  else
+    return x * 2
+  end
+end
+
+print(foo(10)) -- Output: 20
+print(foo(nil)) -- Output: nil
+print(foo('hello')) -- Output: error!
